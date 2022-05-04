ba00fa911d112c73f4e89876021538a08f71d03b
baltazar164 <kuzkem196@gmail.com>
2022-05-04 17:05:22 +0300

    Пром

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1;
+        int rst = -1; //Тест
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
